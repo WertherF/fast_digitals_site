8fda1459e5ff67fe3ded01b4c8e385f8c0763ea8
Werther Fidalgo <106037624+WertherF@users.noreply.github.com>
2024-10-12 12:53:55 -0300

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ const languages = {
         headline: "Boost Your Innovation with Tailor-Made Apps in Record Time",
         slogan: "Personal. Fast. Adaptive.",
         subheadline: "Transform Ideas into Reality: Agile Solutions that Evolve with You",
-        description: "We're Fast Digitals, a trio of tech-loving friends turned co-founders. Our passion? Crafting digital solutions that make people's lives easier and businesses more efficient. We're not just coders; we're problem solvers, dreamers, and your partners in innovation. With a blend of cutting-edge tech and a deep understanding of user needs, we're here to transform your ideas into reality. Join us on this exciting journey of fast-paced, user-centered digital evolution!",
+        aboutUsTitle: "Who We Are: Your Digital Innovation Team",
+        aboutUsContent: "Hello! We are FastDigital, a trio of tech-loving friends turned co-founders with a mission: To transform your ideas into amazing digital solutions, in record time. Our passion? Creating apps that not only make people's lives easier but revolutionize business efficiency. What defines us: • Agility: From concept to launch in weeks, not months. • Customization: Each solution is unique, just like your business. • Innovation: We use cutting-edge technology to solve complex challenges. We're not just developers. We're your innovation partners, turning challenges into digital opportunities. Ready to accelerate your digital journey? Let's create the future together!",
         approachTitle: "Our Approach: The Double Diamond",
         approachDescription: "Continuous Discovery & Continuous Delivery",
         processTitle: "Our Process: Iterative Excellence",
@@ -15,15 +16,14 @@ const languages = {
         getStartedBtn: "Get Your Solution",
         whatsappText: "WhatsApp",
         emailText: "Send Email",
-        additionalText: "From concept to launch in weeks, not months. See how we can accelerate your digital transformation.",
-        aboutUsTitle: "Who We Are: Your Digital Innovation Team",
-        aboutUsContent: "Hello! We are FastDigital, a trio of tech-loving friends turned co-founders with a mission: To transform your ideas into amazing digital solutions, in record time. Our passion? Creating apps that not only make people's lives easier but revolutionize business efficiency. What defines us: • Agility: From concept to launch in weeks, not months. • Customization: Each solution is unique, just like your business. • Innovation: We use cutting-edge technology to solve complex challenges. We're not just developers. We're your innovation partners, turning challenges into digital opportunities. Ready to accelerate your digital journey? Let's create the future together!"
+        additionalText: "From concept to launch in weeks, not months. See how we can accelerate your digital transformation."
     },
     pt: {
         headline: "Impulsione Sua Inovação com Apps Sob Medida em Tempo Recorde",
         slogan: "Personalizado. Ágil. Flexível.",
         subheadline: "Transforme Ideias em Realidade: Soluções Ágeis que Evoluem com Você",
-        description: "Somos a Fast Digitals, um trio de amigos apaixonados por tecnologia que se tornaram co-fundadores. Nossa paixão? Criar soluções digitais que facilitem a vida das pessoas e tornem as empresas mais eficientes. Não somos apenas programadores; somos solucionadores de problemas, sonhadores e seus parceiros em inovação. Com uma combinação de tecnologia de ponta e uma profunda compreensão das necessidades dos usuários, estamos aqui para transformar suas ideias em realidade. Junte-se a nós nesta emocionante jornada de evolução digital rápida e centrada no usuário!",
+        aboutUsTitle: "Quem Somos: Sua Equipe de Inovação Digital",
+        aboutUsContent: "Olá! Somos a FastDigital, um trio de amigos apaixonados por tecnologia que se tornaram co-fundadores com uma missão: Transformar suas ideias em soluções digitais incríveis, em tempo recorde. Nossa paixão? Criar apps que não só facilitam a vida das pessoas, mas revolucionam a eficiência das empresas. O que nos define: • Agilidade: Do conceito ao lançamento em semanas, não meses. • Personalização: Cada solução é única, assim como o seu negócio. • Inovação: Usamos tecnologia de ponta para resolver desafios complexos. Não somos apenas desenvolvedores. Somos seus parceiros em inovação, transformando desafios em oportunidades digitais. Prontos para acelerar sua jornada digital? Vamos criar o futuro juntos!",
         approachTitle: "Nossa Abordagem: O Diamante Duplo",
         approachDescription: "Descoberta Contínua & Entrega Contínua",
         processTitle: "Nosso Processo: Excelência Iterativa",
@@ -34,15 +34,14 @@ const languages = {
         getStartedBtn: "Receba Sua Solução",
         whatsappText: "WhatsApp",
         emailText: "Enviar E-mail",
-        additionalText: "Desde o conceito até o lançamento em semanas, não meses. Veja como podemos acelerar sua transformação digital.",
-        aboutUsTitle: "Quem Somos: Sua Equipe de Inovação Digital",
-        aboutUsContent: "Olá! Somos a FastDigital, um trio de amigos apaixonados por tecnologia que se tornaram co-fundadores com uma missão: Transformar suas ideias em soluções digitais incríveis, em tempo recorde. Nossa paixão? Criar apps que não só facilitam a vida das pessoas, mas revolucionam a eficiência das empresas. O que nos define: • Agilidade: Do conceito ao lançamento em semanas, não meses. • Personalização: Cada solução é única, assim como o seu negócio. • Inovação: Usamos tecnologia de ponta para resolver desafios complexos. Não somos apenas desenvolvedores. Somos seus parceiros em inovação, transformando desafios em oportunidades digitais. Prontos para acelerar sua jornada digital? Vamos criar o futuro juntos!"
+        additionalText: "Desde o conceito até o lançamento em semanas, não meses. Veja como podemos acelerar sua transformação digital."
     },
     es: {
         headline: "Impulse Su Innovación con Apps a Medida en Tiempo Récord",
         slogan: "Personal. Rápido. Adaptativo.",
         subheadline: "Transforme Ideas en Realidad: Soluciones Ágiles que Evolucionan con Usted",
-        description: "Somos Fast Digitals, un trío de amigos amantes de la tecnología convertidos en cofundadores. ¿Nuestra pasión? Crear soluciones digitales que faciliten la vida de las personas y hagan que las empresas sean más eficientes. No somos solo programadores; somos solucionadores de problemas, soñadores y tus socios en innovación. Con una combinación de tecnología de vanguardia y una profunda comprensión de las necesidades de los usuarios, estamos aquí para transformar tus ideas en realidad. ¡Únete a nosotros en este emocionante viaje de evolución digital rápida y centrada en el usuario!",
+        aboutUsTitle: "Quiénes Somos: Su Equipo de Innovación Digital",
+        aboutUsContent: "¡Hola! Somos FastDigital, un trío de amigos apasionados por la tecnología que se convirtieron en cofundadores con una misión: Transformar sus ideas en increíbles soluciones digitales en tiempo récord. ¿Nuestra pasión? Crear aplicaciones que no solo faciliten la vida de las personas, sino que revolucionen la eficiencia de las empresas. Lo que nos define: • Agilidad: Del concepto al lanzamiento en semanas, no meses. • Personalización: Cada solución es única, como su negocio. • Innovación: Utilizamos tecnología de vanguardia para resolver desafíos complejos. No somos solo desarrolladores. Somos sus socios en innovación, transformando desafíos en oportunidades digitales. ¿Listos para acelerar su viaje digital? ¡Creemos el futuro juntos!",
         approachTitle: "Nuestro Enfoque: El Doble Diamante",
         approachDescription: "Descubrimiento Continuo & Entrega Continua",
         processTitle: "Nuestro Proceso: Excelencia Iterativa",
@@ -53,9 +52,7 @@ const languages = {
         getStartedBtn: "Obtenga Su Solución",
         whatsappText: "WhatsApp",
         emailText: "Enviar Correo",
-        additionalText: "Desde el concepto hasta el lanzamiento en semanas, no meses. Vea cómo podemos acelerar su transformación digital.",
-        aboutUsTitle: "Quiénes Somos: Su Equipo de Innovación Digital",
-        aboutUsContent: "¡Hola! Somos FastDigital, un trío de amigos apasionados por la tecnología que se convirtieron en cofundadores con una misión: Transformar sus ideas en increíbles soluciones digitales en tiempo récord. ¿Nuestra pasión? Crear aplicaciones que no solo faciliten la vida de las personas, sino que revolucionen la eficiencia de las empresas. Lo que nos define: • Agilidad: Del concepto al lanzamiento en semanas, no meses. • Personalización: Cada solución es única, como su negocio. • Innovación: Utilizamos tecnología de vanguardia para resolver desafíos complejos. No somos solo desarrolladores. Somos sus socios en innovación, transformando desafíos en oportunidades digitales. ¿Listos para acelerar su viaje digital? ¡Creemos el futuro juntos!"
+        additionalText: "Desde el concepto hasta el lanzamiento en semanas, no meses. Vea cómo podemos acelerar su transformación digital."
     }
 };
 
@@ -70,7 +67,7 @@ const processSteps = {
     pt: [
         { icon: "fas fa-lightbulb", title: "Idealizar", description: "Criar soluções inovadoras" },
         { icon: "fas fa-pencil-alt", title: "Projetar", description: "Criar interfaces centradas no usuário" },
-        { icon: "fas fa-code", title: "Desenvolver", description: "Construir aplicações robustas e escaláveis" },
+        { icon: "fas fa-code", title: "Desenvolver", description: "Construir aplicações robustas  e escaláveis" },
         { icon: "fas fa-rocket", title: "Lançar", description: "Implantar e monitorar sua solução" }
     ],
     es: [
@@ -116,7 +113,8 @@ function changeLanguage(lang) {
     document.getElementById('headline').textContent = data.headline;
     document.getElementById('header-slogan').textContent = data.slogan;
     document.getElementById('subheadline').textContent = data.subheadline;
-    document.getElementById('description').textContent = data.aboutUsContent; // Updated line
+    document.getElementById('about-us-title').textContent = data.aboutUsTitle;
+    document.getElementById('about-us-content').textContent = data.aboutUsContent;
     document.getElementById('approach-title').textContent = data.approachTitle;
     document.getElementById('approach-description').textContent = data.approachDescription;
     document.getElementById('process-title').textContent = data.processTitle;
@@ -132,16 +130,13 @@ function changeLanguage(lang) {
     document.getElementById('secondary-email-text').textContent = data.emailText;
     document.getElementById('footer-slogan').textContent = data.slogan;
     document.getElementById('additional-text').textContent = data.additionalText;
-    document.getElementById('about-us-title').textContent = data.aboutUsTitle;
-    //Removed this line: document.getElementById('about-us-content').textContent = data.aboutUsContent;
-
 
     renderProcessSteps(lang);
     renderReasons(lang);
 
     // Update active state for language buttons
     document.querySelectorAll('.lang-btn').forEach(btn => {
-        btn.classList.toggle('active', btn.textContent.toLowerCase() === lang); // Updated line
+        btn.classList.toggle('active', btn.textContent.toLowerCase() === lang.toUpperCase());
     });
 }
 
